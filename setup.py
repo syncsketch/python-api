870f86e11d3961e61a59edf6cb886777cb1a3acf
Brady Endres <bradyendres@Bradys-MacBook-Pro.local>
2020-05-28 12:40:21 -0700

    update setup.py version and formatting

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 # @Author: yafes
 # @Date:   2018-11-20 17:36:16
-# @Last Modified by:   Brady endres
-# @Last Modified time: 2020-05-27 17:39:39
+# @Last Modified by:   Brady Endres
+# @Last Modified time: 2020-05-28 17:39:39
 
 from setuptools import setup, find_packages
 
-setup(name='syncsketch',
-      version='1.0.7',
-      description='SyncSketch Python API',
-      author='Philip Floetotto',
-      author_email='phil@syncsketch.com',
-      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-      install_requires=[
-                'requests>=2.20.0',
-            ],
-      license='LICENSE.txt',
-    )
+setup(
+    name="syncsketch",
+    version="1.0.7.1",
+    description="SyncSketch Python API",
+    author="Philip Floetotto",
+    author_email="phil@syncsketch.com",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=["requests>=2.20.0"],
+    license="LICENSE.txt",
+)
