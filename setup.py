4666a4e1d9e2ffb822627be5946afb7881739a50
Brady Endres <bradyendres@Bradys-MacBook-Pro.local>
2020-07-10 11:51:41 -0700

    update setup.py version and modified date

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 # @Author: yafes
 # @Date:   2018-11-20 17:36:16
 # @Last Modified by:   Brady Endres
-# @Last Modified time: 2020-05-28 17:39:39
+# @Last Modified time: 2020-07-10
 
 from setuptools import setup, find_packages
 
 setup(
     name="syncsketch",
-    version="1.0.7.1",
+    version="1.0.7.4",
     description="SyncSketch Python API",
     author="Philip Floetotto",
     author_email="phil@syncsketch.com",
