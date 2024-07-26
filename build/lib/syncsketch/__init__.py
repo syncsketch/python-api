2526c2277c07b2954738d06a1db933789223e610
Brady Endres <brady@syncsketch.com>
2024-07-25 17:33:31 -0700

    add sphinx docs. format all methods to produce nice looking docs

--- build/lib/syncsketch/__init__.py
+++ build/lib/syncsketch/__init__.py
@@ -2,12 +2,12 @@
 # @Author: yafes
 # @Date:   2018-11-20 17:39:54
 # @Last Modified by:   Brady Endres
-# @Last Modified time: 2023-04-04
+# @Last Modified time: 2024-07-25
 
 from __future__ import absolute_import
 
 from .syncsketch import SyncSketchAPI
 
-__version__ = "1.0.10.4"
+__version__ = "1.0.10.5"
 __author__ = "SyncSketch Dev Team"
 __credits__ = "Philip Floetotto, Yafes Sahin, Brady Endres, Eric Palakovich Carr"
